66faadbcc835965adf3a21538789613b117905f2
dancema <59177625+dancema@users.noreply.github.com>
2022-01-07 15:55:07 +0100

    replace browserrouter by hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import QuizPage from "./QuizPage";
 
@@ -6,12 +6,12 @@ function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-amber-400">
       <main className="lg:w-1/3 w-11/12 m-5 lg:h-2/3 h-5/6 m-auto py-12 px-4 sm:px-6 lg:px-8 bg-gray-50 rounded-lg drop-shadow-md">
-        <Router>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/category/:id" element={<QuizPage />} />
           </Routes>
-        </Router>
+        </HashRouter>
       </main>
     </div>
   );
