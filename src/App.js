44e8ff6b5de387cd304c8cabaaca084fffd38a81
dancema <59177625+dancema@users.noreply.github.com>
2022-01-16 16:52:16 +0100

    css min height change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import QuizPage from "./QuizPage";
 function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-amber-400">
-      <main className="lg:w-1/3 w-11/12 m-5 lg:h-2/3 h-5/6 m-auto py-12 px-4 sm:px-6 lg:px-8 bg-gray-50 rounded-lg drop-shadow-md">
+      <main className="lg:w-1/3 w-11/12 m-5 min-h-2/3 m-auto py-12 px-4 sm:px-6 lg:px-8 bg-gray-50 rounded-lg drop-shadow-md">
         <HashRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
